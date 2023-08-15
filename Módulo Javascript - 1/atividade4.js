5083c317a2d227a732ca88b50fcbd816928c4b1a
lucas-jorge <21230436+lucas-jorge@users.noreply.github.com>
2023-08-15 09:11:47 -0300

    cadastro de pessoas

---
+++ Módulo Javascript - 1/atividade4.js
@@ -0,0 +1,16 @@
+/*Vamos criar um cadastro de pessoa com os seguintes dados: nome, salário, idade e se possui diploma. Antes de começar a codificação, identifiquem os tipos de cada variável. Em seguida, desenvolvam o código utilizando exemplos para cada variável que respeitem os tipos identificados. Certifiquem-se de que os valores correspondam aos tipos de dados corretos.*/
+
+let nome = "Bruna";
+let salario = 3650;
+let idade = 20;
+let possuiDiploma = false;
+
+console.log(typeof(nome));
+console.log(typeof(salario));
+console.log(typeof(idade));
+console.log(typeof(possuiDiploma));
+
+console.log("Nome: " + nome);
+console.log("Salário: " + salario);
+console.log("Idade: " + idade);
+console.log("Possui diploma: " + possuiDiploma);
