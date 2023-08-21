ed7220d7f8acf1f6b619033d30b879cda9c45296
lucas-jorge <21230436+lucas-jorge@users.noreply.github.com>
2023-08-21 16:15:16 -0300

    palindromo

--- atividadesAula/palindromo.js
+++ atividadesAula/palindromo.js
@@ -1,22 +1,16 @@
-//digite alguns exemplos de frases palíndromas e não palíndromas no console do navegador.
-
 //função que retorna se a frase é palindroma ou não com true ou false no console.
- function palindromo(frase) {
-     if (frase.length <= 1) {
-         return true;
-     }
-     if (frase[0] !== frase[frase.length - 1]) {
-         return false;
-     }
-     return palindromo(frase.slice(1, frase.length - 1));
+const prompt = require("prompt-sync")({ sigint: true });
+
+let palavra = prompt("Digite uma palavra: ");
+
+function palindromo(palavra) {
+    if (palavra.length <= 1) {
+        return true;//é palíndromo.
+    }
+    if (palavra[0] !== palavra[palavra.length - 1]) {
+        return false;//não é palíndromo.
+    }
+    return palindromo(palavra.slice(1, palavra.length - 1));
 }
-console.log(palindromo("arara"));
-console.log(palindromo("desenvolvimento"));
-console.log(palindromo("reviver"));
-console.log(palindromo("radar"));
-console.log(palindromo("reter"));
-console.log(palindromo("comida"));
-console.log(palindromo("torre"));
-console.log(palindromo("ana"));
-console.log(palindromo("osso"));
-console.log(palindromo("ovo"));
+
+console.log(palindromo(palavra));
