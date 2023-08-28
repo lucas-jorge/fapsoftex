6c100d2c4a825b36641c7d358a2db64d60fd6860
lucas-jorge <21230436+lucas-jorge@users.noreply.github.com>
2023-08-28 15:43:05 -0300

    melhorando leitura do código

--- atividadesAula/notasAluno.js
+++ atividadesAula/notasAluno.js
@@ -1,17 +1,18 @@
 //fazer entrada de dados notas de alunos cadastrar alunos e notas, exibir relatório de notas e alunos.exibir aprovado ou nao usando array e switch case.usar for e while.
-//no max 2 notas por aluno
 
+//biblioteca de entrada de dados.
 const prompt = require("prompt-sync")({ sigint: true });
 
-let alunos = [];
+let alunos = [];//array vazio.
 
+//menu para melhorar a usabilidade do código.
 while (true) {
   console.log("Menu:");
   console.log("1 - Cadastrar aluno");
   console.log("2 - Exibir relatório");
   console.log("3 - Sair");
 
-  const opcao = prompt("Escolha uma opção: ");
+  const opcao = prompt("Escolha uma opção: ");//entrada de dados pelo usuário.
 
   switch (opcao) {
     case "1":
@@ -29,6 +30,7 @@ while (true) {
     case "2":
       console.log("Relatório de notas:\n");
 
+      //percorrendo a entrada de dados do aluno no array.
       for (let i = 0; i < alunos.length; i++) {
         relatorio(alunos[i]);
         status(media(alunos[i].notas));
@@ -44,6 +46,7 @@ while (true) {
   }
 }
 
+//função para cadastrar aluno.
 function cadastro(nome, nota1, nota2) {
   return {
     nome: nome,
@@ -51,6 +54,7 @@ function cadastro(nome, nota1, nota2) {
   };
 }
 
+//retornar os dados do aluno cadastrado.
 function relatorio(aluno) {
   console.log(
     `Aluno: ${aluno.nome} - Nota 1: ${aluno.notas[0]} - Nota 2: ${
@@ -59,10 +63,12 @@ function relatorio(aluno) {
   );
 }
 
+//função para calcular médias.
 function media(notas) {
   return (notas[0] + notas[1]) / 2;
 }
 
+//função para retornar se o aluno passou por média.
 function status(media) {
   if (media >= 7) {
     console.log("Aluno aprovado.");
