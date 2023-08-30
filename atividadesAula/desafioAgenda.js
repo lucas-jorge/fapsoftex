816b4f7c899dcc976dfbb23852e16dd46a735d71
lucas-jorge <21230436+lucas-jorge@users.noreply.github.com>
2023-08-30 14:54:46 -0300

    update and cleaned code

--- atividadesAula/desafioAgenda.js
+++ atividadesAula/desafioAgenda.js
@@ -1,15 +1,41 @@
-//crie uma agenda que tenha tres campos e que permita inserir um numero x de vezes pelo teclado
+//crie uma agenda que tenha tres campos e que permita inserir dados e retorne todos os dados de determinada coluna.
 
 const prompt = require('prompt-sync')({sigint: true});
 
+// usando 1 for e switch
+
 let agenda = [];
+let quantidade;
+do {
+    quantidade = parseInt(prompt('Quantas pessoas há na agenda? '));
+} while (isNaN(quantidade) || quantidade <= 0);
 
-for (let i = 0; i < 3; i++) {
-    const nome = prompt('Digite o nome: ');
-    const telefone = prompt('Digite o telefone: ');
-    const email = prompt('Digite o email: ');
-
-    agenda.push([nome, telefone, email]);
+for (let i = 0; i < quantidade; i++) {
+    let pessoa = {
+        nome: prompt('Nome: '),
+        telefone: prompt('Telefone: '),
+        email: prompt('Email: ')
+    };
+    agenda.push(pessoa);
 }
 
-console.log(agenda);
+let opcao = prompt('Deseja buscar por nome, telefone ou email? ').toLowerCase();
+
+let selectedValues = [];
+for (let i = 0; i < agenda.length; i++) {
+    switch (opcao) {
+        case 'nome':
+            selectedValues.push(agenda[i].nome);
+            break;
+        case 'telefone':
+            selectedValues.push(agenda[i].telefone);
+            break;
+        case 'email':
+            selectedValues.push(agenda[i].email);
+            break;
+        default:
+            console.log('Opção inválida.');
+    }
+}
+
+console.log(selectedValues);
