9b4088b9352dfe77b6bc64a8c1ff5dd32d86e6aa
lucas-jorge <lucasajorge@gmail.com>
2023-09-11 15:57:44 -0300

    atividades 11/09

--- atividadesAula/atividadesAula1109/metodoIntegrado.js
+++ atividadesAula/atividadesAula1109/metodoIntegrado.js
@@ -1,4 +1,4 @@
-const pessoa = {};
+/* const pessoa = {};
 pessoa.nome = "Lucas";
 pessoa.sobrenome = "Jorge";
 pessoa.idade = 35;
@@ -9,4 +9,26 @@ pessoa.saudacao = function () {
     return 'Olá, tudo bem? O meu nome é ' + this.nome + ', meu sobrenome é ' + this.sobrenome + ' e tenho ' + this.idade + ' anos.';
 };
 
-console.log(pessoa.saudacao());
+console.log(pessoa.saudacao()); */
+
+function Pessoa(nome_pessoa, idade_pessoa, genero_pessoa) {
+    this.nome = nome_pessoa;
+    this.idade = idade_pessoa;
+    this.genero = genero_pessoa;
+    this.saudacao = function () {
+        return 'Olá, tudo bem? O meu nome é ' + this.nome + '.';
+    };
+}
+
+const pessoa1 = new Pessoa('Lucas', 35, 'H');
+const pessoa2 = new Pessoa('Bruna', 33, 'M');
+
+console.log(pessoa1.nome);
+console.log(pessoa1.idade);
+console.log(pessoa1.genero);
+console.log(pessoa1.saudacao());
+
+console.log(pessoa2.nome);
+console.log(pessoa2.idade);
+console.log(pessoa2.genero);
+console.log(pessoa2.saudacao());
