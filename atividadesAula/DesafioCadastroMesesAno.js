be021e9f35d83606be2e8a2c231790586f8bd1b1
lucas-jorge <lucasajorge@gmail.com>
2023-09-11 14:07:45 -0300

    desafio proposto aula 11/09

--- atividadesAula/DesafioCadastroMesesAno.js
+++ atividadesAula/DesafioCadastroMesesAno.js
@@ -1,77 +1,83 @@
-var meses = ['Jan', 'Fev', 'Mar', 'Abr'];
 const readline = require('readline-sync');
+
+const meses = ['Jan', 'Fev', 'Mar', 'Abr'];
+
 let status = 1;
 
 while (status === 1) {
+  console.log("-------Sistema de cadastro de registro--------");
+  console.log("1 - Inserir");
+  console.log("2 - Alterar");
+  console.log("3 - Excluir");
+  console.log("4 - Listar");
 
-    console.log("-------Sistema de cadastro de registro--------");
+  let op = readline.question("Digite a opção desejada: ");
 
-    let op = readline.question("Digite 1 - Inserir, 2 - Alterar, 3 - Excluir, 4 - Listar: ");
-    switch (op) {
-        case '1':
-            inserirMes();
-            break;
-        case '2':
-            alterar();
-            break;
-        case '3':
-            excluirMes();
-            break;
-        case '4':
-            listarMes();
-            break;
-        default:
-            console.log("Opção inválida.");
-            break;
-    }
+  switch (op) {
+    case '1':
+      inserirMes();
+      break;
+    case '2':
+      alterar();
+      break;
+    case '3':
+      excluirMes();
+      break;
+    case '4':
+      listarMes();
+      break;
+    default:
+      console.log("Opção inválida.");
+      break;
+  }
 
-    status = readline.question("Digite 1 para continuar no Sistema e 2 para sair: ");
+  status = readline.question("Digite 1 para continuar no sistema ou 2 para sair: ");
 }
 
 function inserirMes() {
-    console.log("-------Tela de cadastro de registro--------");
-    let mes = readline.question("Digite o mês: ");
-    meses.push(mes);
-    listarMes();
+  console.log("-------Tela de cadastro de registro--------");
+  let mes = readline.question("Digite o mês: ");
+  meses.push(mes);
+  listarMes();
 }
 
 function listarMes() {
-    console.log("-------Relatório de todos os registros--------");
-    for (var i = 0; i < meses.length; i++) {
-        console.log(meses[i]);
-    }
+  console.log("-------Relatório de todos os registros--------");
+  for (let i = 0; i < meses.length; i++) {
+    console.log(meses[i]);
+  }
 }
 
-function retornaIndice(texto) {
-    let indice = meses.indexOf(texto);
-    return indice;
+function getIndexByText(texto) {
+  let index = meses.indexOf(texto);
+  return index;
 }
 
 function alterar() {
-    console.log("-------Tela para alterar o registro--------");
+  console.log("-------Tela para alterar o registro--------");
+  listarMes();
+  let mesAlterar = readline.question("Escolha o registro a ser alterado: ");
+  let novoValor = readline.question("Informe o novo conteúdo: ");
+  let registro = getIndexByText(mesAlterar);
+  
+  if (registro !== -1) {
+    meses[registro] = novoValor;
     listarMes();
-    var mesAlterar = readline.question("Escolha o registro a ser alterado: ");
-    var novoValor = readline.question("Informe o novo conteúdo: ");
-    var registro = retornaIndice(mesAlterar);
-    
-    if (registro !== -1) {
-        meses[registro] = novoValor;
-        listarMes();
-    } else {
-        console.log("Registro não encontrado.");
-    }
+  } else {
+    console.log("Registro não encontrado.");
+  }
 }
 
 function excluirMes() {
-    console.log("-------Tela para excluir o registro--------");
+  console.log("-------Tela para excluir o registro--------");
+  listarMes();
+  let mesExcluir = readline.question("Escolha o registro a ser excluído: ");
+  let registro = getIndexByText(mesExcluir);
+  
+  if (registro !== -1) {
+    meses.splice(registro, 1);
     listarMes();
-    var mesExcluir = readline.question("Escolha o registro a ser excluído: ");
-    var registro = retornaIndice(mesExcluir);
-    
-    if (registro !== -1) {
-        meses.splice(registro, 1);
-        listarMes();
-    } else {
-        console.log("Registro não encontrado.");
-    }
+  } else {
+    console.log("Registro não encontrado.");
+  }
 }
