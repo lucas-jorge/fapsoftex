f64091ae8df981647321cc7da974867fac8af95b
lucas-jorge <lucasajorge@gmail.com>
2023-10-18 16:06:04 -0300

    adicionando banco de dados SQL

--- projeto6/mySQLScript/scriptSql.sql
+++ projeto6/mySQLScript/scriptSql.sql
@@ -1,10 +1,19 @@
-CREATE TABLE chamados (
+CREATE TABLE Chamado (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    funcionario_nome VARCHAR(255),
-    funcionario_cargo VARCHAR(255),
+    funcionario VARCHAR(255),
+    matricula INT,
+    cargo VARCHAR(255),
     descricao TEXT,
     local VARCHAR(255),
-    data_abertura DATE,
-    data_fechamento DATE,
-    status ENUM('aberto', 'em_execucao', 'fechado')
+    dataAbertura DATE,
+    dataFechamento DATE,
+    status VARCHAR(255)
 );
+--inserindo alguns dados
+
+INSERT INTO Chamado (funcionario, matricula, cargo, descricao, local, dataAbertura, status)
+VALUES ('Lucas Jorge', 123456, 'eletricista', 'Ventilador de teto quebrado no apartamento 601', 'Apartamento 601', CURRENT_DATE, 'Aberto');
+
+-- selecionando os dados
+
+SELECT * FROM Chamado;
