e29c59afde9200410985a689324301934c104c7a
lucas-jorge <lucasajorge@gmail.com>
2023-10-25 15:04:24 -0300

    update

--- bancoDeDados/server.ts
+++ bancoDeDados/server.ts
@@ -89,3 +89,78 @@ app.delete("/excluir/:id", (req, res) => {
         }
     });
 });
+
+/*criando uma tabela de serviços 
+Tabela de Serviço
+
+O servico é identificado pela descrição, local, data de abertura, data de
+fechamento, status (aberto, em execução, fechado). */
+
+app.post("/servico", (req, res) => {
+    const dados = req.body;
+    const sql = "INSERT INTO hotel.servico SET ?";
+    conexao.query(sql, dados, (erro, resultado) => {
+        if (erro) {
+            console.log(erro);
+            res.status(400).json({ erro: erro });
+        } else {
+            res.status(201).json(resultado);
+        }
+    });
+});
+
+//Inserindo os dados
+
+app.get("/servico", (req, res) => {
+    const consulta =
+        "SELECT servico.descricao, servico.local, servico.dataAbertura, servico.dataFechamento, servico.status FROM hotel.servico";
+
+    conexao.query(consulta, (erro, resultado) => {
+        if (erro) {
+            console.log(erro);
+            res.status(400).json({ erro: erro });
+        } else {
+            res.status(200).json(resultado);
+        }
+    });
+});
+
+app.get("/servico/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const sql = "SELECT * FROM hotel.servico WHERE idServico = ?;";
+    conexao.query(sql, id, (erro, resultado) => {
+        if (erro) {
+            console.log(erro);
+            res.status(400).json({ erro: erro });
+        } else {
+            res.status(201).json(resultado);
+        }
+    });
+});
+
+app.put("/servico/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const dados = req.body;
+    const sql = "UPDATE servico SET ? WHERE idServico =?";
+    conexao.query(sql, [dados, id], (erro, resultado) => {
+        if (erro) {
+            console.log(erro);
+            res.status(400).json({ erro: erro });
+        } else {
+            res.status(201).json(resultado);
+        }
+    });
+});
+
+app.delete("/servico/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const sql = "DELETE FROM hotel.servico WHERE idServico =?;";
+    conexao.query(sql, id, (erro, resultado) => {
+        if (erro) {
+            console.log(erro);
+            res.status(400).json({ erro: erro });
+        } else {
+            res.status(201).json(resultado);
+        }
+    });
+});
