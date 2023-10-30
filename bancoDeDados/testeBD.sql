8535c5124ad9df4605b64722262412a30efe081a
lucas-jorge <lucasajorge@gmail.com>
2023-10-30 15:15:30 -0300

    update on sql server

--- bancoDeDados/testeBD.sql
+++ bancoDeDados/testeBD.sql
@@ -1,29 +1,43 @@
 -- Active: 1697656846758@@127.0.0.1@3306@hotel
-CREATE TABLE `chamado` (
-  `idchamado` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(45) NOT NULL,
-  `local` varchar(45) NOT NULL,
-  `data` date NOT NULL,
-  `dataEncerra` date NOT NULL,
-  `status` varchar(45) NOT NULL,
-  `descricao` varchar(45) NOT NULL,
-  `fk_funcionario` int NOT NULL,
+CREATE SCHEMA hotel;
+
+CREATE TABLE `hotel`.`funcionario` (
+  `idfuncionario` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idfuncionario`)
+);
+
+CREATE TABLE `hotel`.`chamado` (
+  `idchamado` INT NOT NULL AUTO_INCREMENT,
+  `descricao` VARCHAR(45) NOT NULL,
+  `data_abertura` DATE NOT NULL,
+  `data_fechamento` DATE NOT NULL,
+  `status` VARCHAR(10) NOT NULL,
+  `fk` INT NOT NULL,
   PRIMARY KEY (`idchamado`),
-  UNIQUE KEY `idchamado_UNIQUE` (`idchamado`),
-  KEY `fk_funcionario_idx` (`fk_funcionario`),
-  CONSTRAINT `fk_funcionario` FOREIGN KEY (`fk_funcionario`) REFERENCES `funcionario` (`idFuncionario`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+  INDEX `fk_idx` (`fk` ASC) VISIBLE,
+  CONSTRAINT `fk`
+    FOREIGN KEY (`fk`)
+    REFERENCES `hotel`.`funcionario` (`idfuncionario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
 
+ALTER TABLE `hotel`.`funcionario`
+  ADD COLUMN `cargo` VARCHAR(45) NOT NULL AFTER `nome`;
 
+INSERT INTO hotel.chamado
+  (descricao, data_abertura, data_fechamento, status, fk)
+VALUES
+  ('Apt 204 Torneira com vazamento', '2023-10-29', '1900-01-01', 'Aberto', 1);
 
-CREATE TABLE `funcionario` (
-  `idFuncionario` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(45) NOT NULL,
-  `cargo` varchar(45) NOT NULL,
-  PRIMARY KEY (`idFuncionario`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+SELECT idchamado, descricao, data_abertura, data_fechamento, status FROM hotel.chamado;
 
-insert into hotel.chamado
-(descricao, data_abertura, data_fechamento, status, fk)
-values ( 'Apt 204 Torneira com vazamento', '2023-10-29', '1900-01-01',
-'Aberto', 1);
+SELECT funcionario.nome, chamado.descricao, chamado.data_abertura, chamado.status
+FROM hotel.funcionario
+INNER JOIN hotel.chamado ON hotel.funcionario.idfuncionario = hotel.chamado.fk;
+
+INSERT INTO hotel.funcionario
+  (nome, cargo, idfuncionario)
+  VALUES
+  ('João', 'Gerente', 1);
